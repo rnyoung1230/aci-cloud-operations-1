87cdf4135caa3cdee51d1ecf1b1a8a41ebc7aa0e
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-02-25 05:53:58 -0600

    Initial commit

---
+++ 1_scripting/bash_shell/add_numbers.py
@@ -0,0 +1,11 @@
+# Ask user to enter a number
+x=int(input("Enter a number\n>"))
+y=int(input("Enter a number\n>"))
+z=int(input("Enter a number\n>"))
+
+# Sum the numbers
+numbers = [x, y, z]
+total = sum(numbers)
+
+# Display a message to console with the calculated sum
+print(f"Python says the answer is {total}.")
